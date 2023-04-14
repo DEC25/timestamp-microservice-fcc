0ad24633a1235091d5469360458a780b00137e3d
Diego Elias <diego_eliasc2505@live.com>
2023-04-14 18:17:36 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/:date?", (req, res) => {
+  let { date } = req.params;
+  const response = { unix: "", utc: "" };
+  
+  if (!date) {
+    let currentUTCTime = new Date().toUTCString();
+    let currentUnixTime = new Date();
+    response.utc = currentUTCTime;
+    response.unix = currentUnixTime;
+    return res.json(response);
+  }
+  
+  date = Number.parseInt(date);
+  
+  if (!date) return res.json({ error: 'Invalid Date' });
+
+  response.unix = date;
+  response.utc = new Date(date).toUTCString();
+
+  res.json(response);
+});
+
+app.listen(PORT, console.log(`server is running on http://localhost:${PORT}`));
