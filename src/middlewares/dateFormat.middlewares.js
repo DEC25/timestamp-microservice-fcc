2070a15e9a1c3086ba5bfec3ad85dec7a7396bf2
Diego Elias <diego_eliasc2505@live.com>
2023-04-23 03:00:29 -0500

    change regex validation for Date.parse()

--- src/middlewares/dateFormat.middlewares.js
+++ src/middlewares/dateFormat.middlewares.js
@@ -2,12 +2,13 @@ const verifyDateFormat = (req, res, next) => {
   let { date } = req.params;
   const ERROR_MESSAGE = { error: 'Invalid Date' };
 
-  if (date.match(/^\d{4}-\d{2}-\d{2}$/)) return next();
+  // if (date.match(/^\d{4}-\d{2}-\d{2}$/)) return next();
+  if (Date.parse(date)) return next();
   if (date.match(/^\d+$/)) {
     req.params.date = Number(date);
     return next();
   }
-  
+
   res.json(ERROR_MESSAGE);
 }
 
