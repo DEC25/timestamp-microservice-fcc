a8fd3353ecaee8ffa4d75a7f6c2e502ce4be4a03
Diego Elias <diego_eliasc2505@live.com>
2023-04-23 02:43:26 -0500

    fixing errors in date format middleware

--- src/middlewares/dateFormat.middlewares.js
+++ src/middlewares/dateFormat.middlewares.js
@@ -2,10 +2,10 @@ const verifyDateFormat = (req, res, next) => {
   let { date } = req.params;
   const ERROR_MESSAGE = { error: 'Invalid Date' };
 
-  if (date.match(/^\d{4}-\d{2}-\d{2}$/)) next();
+  if (date.match(/^\d{4}-\d{2}-\d{2}$/)) return next();
   if (date.match(/^\d+$/)) {
     req.params.date = Number(date);
-    next();
+    return next();
   }
   
   res.json(ERROR_MESSAGE);
